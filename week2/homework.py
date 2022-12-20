a18325a4fd29cbb773d0e712da311de7fe7dba79
Kris Steigerwald <kris.steigerwald@gmail.com>
2022-12-19 19:11:39 -0700

    Signed transaction with txID: W7QCI7SURZWXLHZGOCCKEOFUHLWN6XKPBYWQ7WREOV72V7PMG2VA TXID:  W7QCI7SURZWXLHZGOCCKEOFUHLWN6XKPBYWQ7WREOV72V7PMG2VA Result confimation in round: 26387041

--- week2/homework.py
+++ week2/homework.py
@@ -13,3 +13,32 @@ asset_creator_address="322QW53QB4BNKE2T52XNGD2TYQ22I4I45SMKYLR7IYSG5AYSGC2DOQTMD
 
 private_key = to_private_key(passphrase)
 
+txn = AssetConfigTxn(
+  sender=asset_creator_address,
+  sp=algod_client.suggested_params(),
+  total=1000,
+  default_frozen=False,
+  unit_name="LATINUM",
+  asset_name="VelaruLatinum" ,
+  manager=asset_creator_address,
+  reserve=asset_creator_address,
+  freeze=asset_creator_address,
+  clawback=asset_creator_address,
+  url="https://gateway.pinata.cloud/ipfs/QmRCVewpjF9DCDih2rMcdZ8HaC8WDp3e7pUhyivi8RzqzD",
+  decimals=0) 
+  
+
+signed_txn = txn.sign(private_key)
+try:
+  txid = algod_client.send_transaction(signed_txn)
+  print("Signed transaction with txID: {}".format(txid))  
+  confirmed_txn = wait_for_confirmation(algod_client, txid, 4)
+  print("TXID: ", txid)
+  print("Result confimation in round: {}".format(confirmed_txn['confirmed-round']))
+except Exception as err:
+  print(err)
+  
+  
+  print("Transaction Info: {}".format(
+    json.dumps(confirmed_txn, indent=4)
+  ))
